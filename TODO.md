41dc749a272d59246b0618b616c903d4428dcbe2
Odin Hultgren Van Der Horst <odin@digitalgarden.no>
2021-02-03 10:50:53 +0100

    Added bytea with nullbyte test

--- TODO.md
+++ TODO.md
@@ -3,10 +3,14 @@
 - [ ] Only accept creation of tables with 2 or 3 columns
 - [ ] Make sure transactions fail on connection loss
 - [X] Change from timeout to time/interval/nil reconnect
+- [ ] Err output what column is bad
+- [ ] Use only ktelog
 
 # Tests that need to be written
 - [X] kt_set
 - [X] kt_append
 - [X] Insert value = NULL
+- [X] NULL byte in keys and values
+- [ ] options
 - [ ] Reconnect to kyototycoon
 - [ ] No kt server test
---
+++ test/sql/007-bytea.sql
@@ -0,0 +1,12 @@
+create server kt_server foreign data wrapper kt_fdw;
+create foreign table kt_test (key bytea, value bytea, flag text) server kt_server;
+create user mapping for public server kt_server;
+delete from kt_test;
+insert into kt_test values (E'\\xDEAD00BEEF'::bytea, E'\\xDEAD00BEEF'::bytea);
+select * from kt_test;
+insert into kt_test values (E'\\xDEAD00BEEF'::bytea, 'foo', 'kt_set');
+select * from kt_test;
+update kt_test set value = 'bar' where key::bytea = E'\\xDEAD00BEEF'::bytea;
+select * from kt_test;
+delete from kt_test where key::bytea = E'\\xDEAD00BEEF'::bytea;
+select * from kt_test;
