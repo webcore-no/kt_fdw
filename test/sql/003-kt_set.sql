83da3c439716f4f9f946dc643183a2c67ac51236
Odin Hultgren Van Der Horst <odin@digitalgarden.no>
2021-02-03 12:58:13 +0100

    Fixes to tests

--- test/sql/003-kt_set.sql
+++ test/sql/003-kt_set.sql
@@ -1,3 +1,7 @@
+set client_min_messages=ERROR;
+drop server if exists kt_server cascade;
+set client_min_messages=NOTICE;
+
 create server kt_server foreign data wrapper kt_fdw;
 create foreign table kt_test (key text, value text, flag text) server kt_server;
 create user mapping for public server kt_server;
--- test/sql/007-bytea.sql
+++ test/sql/007-bytea.sql
@@ -1,3 +1,8 @@
+set client_min_messages=ERROR;
+drop server if exists kt_server cascade;
+set client_min_messages=NOTICE;
+
+
 create server kt_server foreign data wrapper kt_fdw;
 create foreign table kt_test (key bytea, value bytea, flag text) server kt_server;
 create user mapping for public server kt_server;
@@ -6,7 +11,7 @@ insert into kt_test values (E'\\xDEAD00BEEF'::bytea, E'\\xDEAD00BEEF'::bytea);
 select * from kt_test;
 insert into kt_test values (E'\\xDEAD00BEEF'::bytea, 'foo', 'kt_set');
 select * from kt_test;
-update kt_test set value = 'bar' where key::bytea = E'\\xDEAD00BEEF'::bytea;
+update kt_test set value = 'bar' where key = E'\\xDEAD00BEEF'::bytea;
 select * from kt_test;
-delete from kt_test where key::bytea = E'\\xDEAD00BEEF'::bytea;
+delete from kt_test where key = E'\\xDEAD00BEEF'::bytea;
 select * from kt_test;
