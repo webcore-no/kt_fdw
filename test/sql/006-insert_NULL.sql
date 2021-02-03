6e970705daa5b2437965394b43cd20ac1de37265
Odin Hultgren Van Der Horst <odin@digitalgarden.no>
2021-02-03 08:59:01 +0100

    Added NULL insert tests

---
+++ test/sql/006-insert_NULL.sql
@@ -0,0 +1,14 @@
+create server kt_server foreign data wrapper kt_fdw;
+create foreign table kt_test (key text, value text, flag text) server kt_server;
+create user mapping for public server kt_server;
+delete from kt_test;
+insert into kt_test values ('key1', NULL);
+select * from kt_test;
+insert into kt_test values ('key1', 'bar');
+select * from kt_test;
+insert into kt_test values ('key1', NULL);
+select * from kt_test;
+insert into kt_test values (NULL, NULL);
+select * from kt_test;
+insert into kt_test values (NULL, 'bar');
+select * from kt_test;
